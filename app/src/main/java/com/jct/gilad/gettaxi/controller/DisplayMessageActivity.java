a61e750fd5e0607c2069b6415d2f0a2d8a240443
gilad1324 <giladngr1324@gmail.com>
2018-12-12 00:13:36 +0200

    vfggf

--- app/src/main/java/com/jct/gilad/gettaxi/controller/DisplayMessageActivity.java
+++ app/src/main/java/com/jct/gilad/gettaxi/controller/DisplayMessageActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.jct.gilad.gettaxi.R;
-import com.jct.gilad.gettaxi.controller.MainActivity;
 
 public class DisplayMessageActivity extends Activity {
 
